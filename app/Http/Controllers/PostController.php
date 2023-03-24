beea1730bf0bdef15984ff177e25970b1db67535
yuji11022 <yujiyamaguchi1102@gmail.com>
2023-03-25 01:06:40 +0900

    First commit of dev_basis02

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index(Post $post)
+    {
+        return view('posts/index')->with(['posts' => $post->getPaginateByLimit(1)]);  
+       //blade内で使う変数'posts'と設定。'posts'の中身にgetを使い、インスタンス化した$postを代入。
+    }
+}
